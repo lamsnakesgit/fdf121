0b0a1893c7e62ce7373540d736086b412e7b6502
lamsnakesgit <cybvvow@gmail.com>
2020-03-11 22:20:41 +0300

    -fbp

--- drawer.c
+++ drawer.c
@@ -17,76 +17,6 @@ float		mod(float a)
 	return (a >= 0 ? a : -a);
 }
 
-void		findpb(t_coord crd, int x1, int y1, t_fdf *fdf)
-{
-	float	x_st;
-	float	y_st;//	int		x_st;//	int		y_st;
-	int		max;
-	int		z;//float
-	int		z1;
-//	float		z;//float
-//	float		z1;
-
-    printf("ZOOM!!!=%d\n", fdf->zoom);
-	z = fdf->z_matrix[(int)crd.y][(int)crd.x] * fdf->zoom / fdf->z_sh;//fdf->zmv;//2;
-	z1 = fdf->z_matrix[(int)y1][(int)x1] * fdf->zoom / fdf->z_sh;//2;
-//	====ZOOM!!!!!!!!!=========
-//	crd.x = (crd.x - fdf->w / 2) * fdf->zoom;
-//	crd.x = (crd.y - fdf->h / 2) * fdf->zoom;
-//	x1 = (x1 - fdf->w / 2) * fdf->zoom;
-//	y1 = (y1 - fdf->h / 2) * fdf->zoom;
-//	z *= fdf->zoom; z1 *= fdf->zoom;//
-	//=========color==============
-	fdf->color = (z || z1) ? RED : WHITE;
-	//==========3D==========ISO
-	if (fdf->projection == ISO)
-	{
-//		z += fdf->z_sh;//SHIFT Z ZOOM
-//		z1 += fdf->z_sh;
-		isometric(&crd.x, &crd.y, z,fdf);
-		isometric(&x1, &y1, z1,fdf);
-	}
-	//			SHIFT==========
-	crd.x += fdf->shift_x; //150;
-	crd.y += fdf->shift_y;//150;
-	x1 += fdf->shift_x;//150;//05;
-	y1 += fdf->shift_y;//150;
-//==ROTATION x
-//	y = y * cos(angle) + z * sin(ANGLE);
-//	z = -y * sin(angle) + z cos(ANGLE);
-//ROTY
-//	x = x * cos(angle) + z * sin(angle);
-//	z = -x * cos(angle) + z * cos(angle);
-//ROTZ
-//	x = x * cos(angle) - y * sin(angle);
-//	y = x * sin(angle) + y * cos(angle);
-	x_st = x1 - crd.x;
-	y_st = y1 - crd.y;
-	if (mod(x_st) > mod(y_st))
-		max = mod(x_st);
-	else
-		max = mod(y_st);
-	x_st /= max;
-	y_st /= max;
-	int i;
-	i = 0;
-//	printf("x_st=%f y_st=%f x=%f y=%f fdcolor=%d\n", x_st, y_st, x, y, fdf->color);
-//	printf("CLR=%d\n", CLR);//65280
-	while ((int)(crd.x - x1)  || (int)(crd.y - y1)  )//? lol no>0 ||//////bresenham
-	{
-	//	i = (int)(fdf->w * (int)crd.y + (int)crd.x);
-	//	printf("i=%d fdf->color=%d\n", i, fdf->color);
-		//	printf("i=%d fdfimgi=%d\n", i, fdf->color);//fdf->img[0]);
-	//	fdf->img[i] = fdf->color;
-//		if (i - 1 >= 0)
-//				printf("WHYimgi=%d\n, fdf->color=%d\n", fdf->img[i-1], fdf->color);
-//		++i;
-		mlx_pixel_put(fdf->mlx_ptr, fdf->win_ptr, crd.x, crd.y, fdf->color);//0xffffff);
-		//	printf("MPPUT=%d y=%d x-x1=%d y-y1=%d\n", x ,y, (int)(x-x1), (int)(y-y1));
-		crd.x += x_st;//1;//x_st;
-		crd.y += y_st;
-	}
-}
 void 		brs(t_coord crd, int x1, int y1, t_fdf *fdf)
 {
 	int dy;
@@ -217,28 +147,23 @@ void 		brs(t_coord crd, int x1, int y1, t_fdf *fdf)
 //            y1 += signY;
 //        }
 //    }
+void        go_blank(t_fdf *fdf)
+{
+    int q;
 
+    q = 0;
+    while (q < WIN_W_X * WIN_W_X * 4)
+    {
+        fdf->img[q] = 0x00;
+        ++q;
+    }
+}
 void		draw_line(t_fdf *fdf)
 {
 	t_coord	crd;
 	int x;
 	int y;
-//	printf("33asdsdfgsfgsdfg\n");
 
-//	ft_blank(fdf->img, fdf->not_my_bus);
-//	printf("asdsdfgsfgsdfg\n");
-//	//ft_bzero(fdf->img, fdf->not_my_bus);
-//	printf("asdsdfgsfgsdfg\n");
-//	//ft_bzero(fdf->img, WIN_W_X);
-//	printf("asdsdfgsfgsdfg\n");
-	int q = 0;
-	int *ar;
-	ar = fdf->img;
-	while (q < WIN_W_X * WIN_W_X * 4)
-	{
-		ar[q] = 0x00;//0x00ffff;
-		++q;
-	}
 	crd.y = 0;
 //	printf("fdf-h=%d fdf->w=%d \n", fdf->h, fdf->w);
     printf("ZEZOOM=%d\n", fdf->zoom);
@@ -250,15 +175,12 @@ void		draw_line(t_fdf *fdf)
 		{
 			if (crd.x < fdf->w - 1)
 				brs(crd, crd.x + 1, crd.y, fdf);
-				//findpb(crd, crd.x + 1, crd.y, fdf);//lol putimg and dissapear
 			//		printf("WER=%f crdy=%f\n", crd.x, crd.y);
 			if (crd.y < fdf->h - 1)
 				brs(crd, crd.x, crd.y + 1, fdf);
-				//findpb(crd, crd.x, crd.y + 1, fdf);
 			++crd.x;
 		}
 		++crd.y;
-//        break ;///
 	}
 	mlx_put_image_to_window(fdf->mlx_ptr, fdf->win_ptr, fdf->img_ptr, 0, 0);
 	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, 50, 50, WHITE, "P - parallel I - ISO\nnumpad - rotate\n");
